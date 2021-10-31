512ef9ba193e618b71f6e63393ac954ce9fc6e11
tst <cpastoast@protonmail.com>
2021-10-31 10:34:45 +0100

    Updated CRTSh module
    Now parsing json instead of using a regex.

--- modules/crtsh.py
+++ modules/crtsh.py
@@ -12,12 +12,38 @@ class CRTSh(Module):
         self.target = target
 
 
+    def add_subdomain(self, subdomain: Subdomain):
+        """ Add a subdomain to the database. """
+        
+        """ 
+        Filter things like '*.domain.com', 
+        3rd party domains or same as the main domain. 
+        """
+        
+        if (
+            not str(subdomain).endswith(self.target)
+            or str(subdomain).startswith("*")
+            or str(subdomain) == self.target
+        ):
+            return
+
+        database.add_subdomain(subdomain)
+
+
     def run(self):
         response = requests.get("https://crt.sh?q=%s&output=json" % self.target)
 
-        subdomains = set(re.findall(r'\"([a-zA-Z0-9.-]+\.%s)\"' % self.target, response.text))
+        json_data = response.json()
 
-        for subdomain in subdomains:
-            subdomain =  Subdomain(subdomain)
-
-            database.add_subdomain(subdomain)
+        for report in json_data:
+            subdomain = Subdomain(report["name_value"])
+            
+            if "\n" in str(subdomain):
+                parts = str(subdomain).split("\n")
+                
+                for part in parts:
+                    self.add_subdomain(Subdomain(part))
+                    
+                continue
+            
+            self.add_subdomain(subdomain)
