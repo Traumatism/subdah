5ade0f73b52d7ad857fb3ebd81555c01176a7124
tst <cpastoast@protonmail.com>
2021-11-25 19:08:06 +0100

    updated banner

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "2.0.0"
 
 from .database import Database
-from .logger import console
+from .logger import Logger, console
 
-database = Database() # database containing all subdomains.
+database = Database()  # database containing all subdomains.
